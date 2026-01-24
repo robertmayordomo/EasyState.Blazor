792aa167478952a8fe7de4e1543a2eb448fe1697
robertmayordomo <robert@mayordomo.co.uk>
2026-01-24 20:37:24 +0000

    Add async UpdateState and Dispose to IAppState interface
    Added an async UpdateState method accepting Func<T, Task> for asynchronous state updates. Introduced Dispose method for explicit resource cleanup in IAppState implementations.

--- EasyState.Blazor/IAppState.cs
+++ EasyState.Blazor/IAppState.cs
@@ -5,5 +5,7 @@ public interface IAppState
     T GetState<T>() where T : class, new();
     Task SetState<T>(T state) where T : class;
     Task UpdateState<T>(Action<T> updateAction) where T : class, new();
+    Task UpdateState<T>(Func<T, Task> updateAction) where T : class, new();
     IObservable<T> ObserveState<T>() where T : class, new();
+    void Dispose();
 }
